3ffc1aebcf9fe52e278afaaf1ff1d8dc9fc04f9d
Vito Karyadi <77707067+VitoKaryadi@users.noreply.github.com>
2024-01-09 21:23:29 +0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -26,12 +26,12 @@ export default defineAppConfig({
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'vitokaryadi',
+      instagram: 'vitokaryadi_',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        href: 'https://linkedin.com/in/vitokaryadi'
       }
     },
     form: {
